43221dc0dd001253bd1609f5068e9deaa292b198
Egor Vasyakin <evasyakin@mail.ru>
2020-02-01 23:15:49 +0700

    Create Controller.php

---
+++ src/Controller.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package evas-php/evas-mvc
+ */
+namespace Evas\Mvc;
+
+use Evas\Base\App;
+
+/**
+ * Константы для свойств класса по умолчанию.
+ */
+if (!defined('EVAS_VIEW_PATH')) define('EVAS_VIEW_PATH', 'view/');
+
+/**
+ * Controller.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class Controller
+{
+    /**
+     * @var string директория файлов отображения
+     */
+    public $viewPath = EVAS_VIEW_PATH;
+
+    /**
+     * Получение полного имени файла.
+     * @param string относительный путь к файлу
+     * @return string полный путь к файлу
+     */
+    public function filename(string $filename): string
+    {
+        return App::getDir() . $this->viewPath . $filename;
+    }
+
+    /**
+     * Рендер файла/шаблона.
+     * @param string имя файла/шаблона
+     * @param array аргументы для файла
+     * @throws Evas\Base\Exception\FileNotFoundException
+     * @return mixed|null возвращаемый результат файла
+     */
+    public function render(string $filename, array $args = [])
+    {
+        return App::load($this->filename($filename), $args);
+    }
+
+    /**
+     * Проверка на возможность рендера.
+     * @param string имя файла/шаблона
+     * @return bool удалось ли прочитать файл
+     */
+    public function canRender(string $filename): bool
+    {
+        return App::canLoad($this->filename($filename));
+    }
+}
